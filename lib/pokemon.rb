de452402e28fc2dffc608b6ef6f70527375ffd10
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-01 19:38:46 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -12,7 +12,7 @@ attr_reader :name, :type, :db, :id
   end
 
   def self.find(num, db)
-    db.execute("SELECT * FROM pokemon")
+    db.execute("SELECT name, type FROM pokemon")
   end
 
   def self.id
