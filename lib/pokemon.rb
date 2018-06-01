11a5d13fde72595773de85e2077b24d521a0996b
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-01 19:53:48 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -13,6 +13,7 @@ attr_accessor :name, :type, :db, :id
   end
 
   def self.find(num, db)
-    db.execute("SELECT * FROM pokemon WHERE pokemon.id = ?",num)
+    new_pokemon = db.execute("SELECT * FROM * WHERE pokemon.id = ?",num)
+    binding.pry
   end
 end
